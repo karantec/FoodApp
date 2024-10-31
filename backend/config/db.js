db464d8edd9a978aaefb8face93252e36e6e7042
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-11-01 00:22:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://karanrana3095:lpKce0r5Yi0GLhO6@cluster0.cuvnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+    await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
        console.log('DB connected') ;
     })
 }
